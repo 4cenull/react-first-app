1bff47b4de9b83c1a60922e6870d093e10aebd62
4ceNull <4ceNull@protonmail.com>
2021-08-17 17:30:49 +0900

    [DELETE] App.cssの削除 [CHANGE] App.jsのcss(App.css)をmaterial-uiのmakeStylesで実装し直した

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+  //camelCase
+  app: {
+    textAlign: 'center'
+  },
+  appHeader: {
+    backgroundColor: '#282c34',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 'calc(10px + 2vmin)',
+    color: 'white'
+  },
+  appLink: {
+    color: '#61dafb'
+  },
+  appLogo: {
+    animation: '$App-logo-spin infinite 20s linear',
+    height: '40vmin',
+    pointeEvents: 'none'
+  },
+  '@keyframes App-logo-spin': {
+    from: {
+      transform: 'rotate(0deg)'
+    },
+    to: {
+      transform: 'rotate(360deg)'
+    }
+  }
+});
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={classes.app}>
+      <header className={classes.AppHeader}>
+        <img src={logo} className={classes.appLogo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={classes.appLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
